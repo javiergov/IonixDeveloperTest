7debd12aad7656b747da448234e43d4dd14cf98d
Javier González O <javiergov+dev@gmail.com>
2020-08-28 17:38:19 -0400

    Initial Commit

---
+++ Ionix Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ionix Test
+//
+//  Created by Javier González Ovalle on 28-08-20.
+//  Copyright © 2020 Gov Elo Works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
