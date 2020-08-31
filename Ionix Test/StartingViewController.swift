d50e824766557241bef08d9e72986bee7d107e7f
Javier González O <javiergov+dev@gmail.com>
2020-08-31 06:28:18 -0400

    • Adds background image into xcassets. • Adds a new view controller to avoid using storyboards as requested by the specification.

---
+++ Ionix Test/StartingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartingViewController.swift
+//  Ionix Test
+//
+//  Created by Javier González Ovalle on 30-08-20.
+//  Copyright © 2020 Gov Elo Works. All rights reserved.
+//
+
+import UIKit
+
+class StartingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
